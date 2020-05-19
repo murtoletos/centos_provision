a80872c8b86a0ff2c31f0cbf63a9fadeef9d5f53
Artur Sabirov <artur.sabirov@apliteni.com>
2020-05-19 07:10:29 +0200

    knapsack_pro

--- scripts/spec/spec_helper.rb
+++ scripts/spec/spec_helper.rb
@@ -11,7 +11,13 @@ require 'byebug'
 require 'inventory'
 require 'script'
 
+if ENV['CI']
+  require 'knapsack'
+  Knapsack::Adapters::RspecAdapter.bind
+end
+
 RSpec.configure do |config|
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
