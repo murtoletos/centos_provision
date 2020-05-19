84a23f429b4740e1e957d023a813b2485d67d1d7
Artur Sabirov <artur.sabirov@apliteni.com>
2020-05-19 07:48:13 +0200

    revert

--- scripts/spec/scripts/shared_contexts/run_script_in_tmp_dir.rb
+++ scripts/spec/scripts/shared_contexts/run_script_in_tmp_dir.rb
@@ -1,20 +1,9 @@
 require 'shell'
 
 RSpec.shared_context 'run script in tmp dir', run_script_in_tmp_dir: :yes do
-  before do
-    if ENV['HOME_DIR']
-      @tmp_dir = Dir.mktmpdir('', "tmp")
-      @copy_dir = Dir.pwd + '/' + @tmp_dir
-      dir = ENV['HOME_DIR']
-      @current_dir = "#{dir}/#{@tmp_dir}"
-    else
-      @current_dir = @tmp_dir = @copy_dir = Dir.mktmpdir('', '/tmp')
-    end
-  end
-  before { @old_current_dir = Dir.pwd; Dir.chdir(@copy_dir) }
-  before do
-    FileUtils.copy("#{ROOT_PATH}/#{subject.script_command}", @copy_dir)
-  end
+  before { @current_dir = Dir.mktmpdir('', '/tmp') }
+  before { @old_current_dir = Dir.pwd; Dir.chdir(@current_dir) }
+  before { FileUtils.copy("#{ROOT_PATH}/#{subject.script_command}", @current_dir) }
 
   after { Dir.chdir(@old_current_dir) }
   after { FileUtils.rm_rf(@current_dir) }
@@ -22,11 +11,11 @@ RSpec.shared_context 'run script in tmp dir', run_script_in_tmp_dir: :yes do
   def run_script(inventory_values: {installer_version: Script::INSTALLER_RELEASE_VERSION})
     if defined?(copy_files)
       copy_files.each do |path_to_file|
-        FileUtils.copy(path_to_file, @copy_dir)
+        FileUtils.copy(path_to_file, @current_dir)
       end
     end
     Inventory.write(inventory_values)
-    subject.call(current_dir: "#{@current_dir}")
+    subject.call(current_dir: @current_dir)
     @inventory = Inventory.read_from_log(subject.log)
   end
 end
