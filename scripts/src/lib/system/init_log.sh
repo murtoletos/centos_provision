6838470e0cbefc3c20ad42eedd0fae311c682e08
Dmitry Lihachev <dmitry.lihachev@apliteni.com>
2020-05-25 13:06:12 +0700

    INST-135 Keep old log files

--- scripts/src/lib/system/init_log.sh
+++ scripts/src/lib/system/init_log.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-init_log(){
+KEEP_LOGS_COUNT=10
+
+init_log() {
   if mkdir -p ${WORKING_DIR} &> /dev/null; then
+    save_previous_log
+    delete_old_logs
     > ${SCRIPT_LOG}
   else
     echo "Can't create keitaro working dir ${WORKING_DIR}" >&2
     exit 1
   fi
+}
+
+save_previous_log() {
+  if [[ -f "${SCRIPT_LOG}" ]]; then
+    local datetime_of_script_log=$(date -r "${SCRIPT_LOG}" +"%Y%m%d%H%M%S")
+    mv "${SCRIPT_LOG}" "${WORKING_DIR}/${SCRIPT_LOG}-${datetime_of_script_log}"
+  fi
+}
+
+delete_old_logs() {
+  for old_log in $(find "${WORKING_DIR}" -name "${SCRIPT_LOG}-*" | sort | head -n-${KEEP_LOGS_COUNT}); do
+    debug "Deleting old log ${old_log}"
+    rm -f "${old_log}"
+  done
 }
