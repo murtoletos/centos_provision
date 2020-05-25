4935568ff70044c9e48ff4851c480a9e1a24759c
Artur Sabirov <artur.sabirov@apliteni.com>
2020-05-25 08:27:57 +0200

    fix

--- scripts/src/app/installer/stage6/download_provision.sh
+++ scripts/src/app/installer/stage6/download_provision.sh
@@ -3,6 +3,6 @@
 download_provision(){
   debug "Download playbook.zip"
 
-  url="${PLAYBOOK_URL:-DEFAULT_PLAYBOOK_URL}"
+  url="${PLAYBOOK_URL:-$DEFAULT_PLAYBOOK_URL}"
   run_command "curl -fsSL ${url} && unzip -qo playbook.zip && ls"
 }
