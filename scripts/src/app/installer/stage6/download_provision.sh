b13026383edcf6e10bf9eaaefa34540c4ae5dad3
Artur Sabirov <artur.sabirov@apliteni.com>
2020-05-25 09:20:39 +0200

    install unzip

--- scripts/src/app/installer/stage6/download_provision.sh
+++ scripts/src/app/installer/stage6/download_provision.sh
@@ -3,6 +3,8 @@
 download_provision(){
   debug "Download playbook.zip"
 
+  run_command "yum install -y unzip"
+
   url="${PLAYBOOK_URL:-$DEFAULT_PLAYBOOK_URL}"
-  run_command "curl -fsSL ${url} > pllaybook.zip && unzip -qo playbook.zip && rm playbook.zip"
+  run_command "curl -fsSL ${url} > playbook.zip && unzip -qo playbook.zip && rm playbook.zip"
 }
