2a760fb24533bdbd9f77d69e95829a40806992d1
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2020-07-29 01:17:33 +0300

    INST-192 test

--- scripts/src/app/installer/stage6/download_provision.sh
+++ scripts/src/app/installer/stage6/download_provision.sh
@@ -3,6 +3,6 @@
 download_provision(){
   debug "Download provision"
   release_url="https://files.keitaro.io/scripts/${BRANCH}/playbook.tar.gz"
-  mkdir -p "${PROVISION_DIRECTORY}-${BRANCH}"
-  run_command "curl -fsSL ${release_url} | tar -xzC ${PROVISION_DIRECTORY}-${BRANCH}"
+  mkdir -p "${PROVISION_DIRECTORY}"
+  run_command "curl -fsSL ${release_url} | tar -xzC ${PROVISION_DIRECTORY}"
 }
