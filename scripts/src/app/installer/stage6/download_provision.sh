d4c49dfce83852916611fec77239de3b4f95f30b
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2020-07-28 21:59:53 +0300

    INST-192 temp

--- scripts/src/app/installer/stage6/download_provision.sh
+++ scripts/src/app/installer/stage6/download_provision.sh
@@ -3,5 +3,7 @@
 download_provision(){
   debug "Download provision"
   release_url="https://github.com/apliteni/centos_provision/archive/${BRANCH}.tar.gz"
-  run_command "curl -fsSL ${release_url} | tar xz"
+  curl -fsSL https://github.com/apliteni/centos_provision/archive/INST-189.tar.gz | tar xz
+  curl -fsSL https://files.keitaro.io/scripts/builds/INST-189/playbook.zip | tar xz
+  run_command "curl -fsSLO ${release_url} | unzip"
 }
