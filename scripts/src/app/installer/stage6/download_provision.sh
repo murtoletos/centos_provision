d6781515d2c98e151a3c2d0e7766c119fa7b046a
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2020-07-28 23:19:35 +0300

    INST-192 temp

--- scripts/src/app/installer/stage6/download_provision.sh
+++ scripts/src/app/installer/stage6/download_provision.sh
@@ -2,7 +2,6 @@
 
 download_provision(){
   debug "Download provision"
-  release_url="https://github.com/apliteni/centos_provision/archive/${BRANCH}.tar.gz"
-  release_url="https://files.keitaro.io/scripts/${BRANCH}/playbook.tag.gz"
+  release_url="https://files.keitaro.io/scripts/${BRANCH}/playbook.tar.gz"
   run_command "curl -fsSL ${release_url} | tar xz"
 }
